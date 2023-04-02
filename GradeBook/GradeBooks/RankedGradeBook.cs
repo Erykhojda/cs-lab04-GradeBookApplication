49597d0b432cfdeb90e897993b832722b8343769
Eryk <eryk.hojda@microsoft.wsei.edu.pl>
2023-04-02 15:25:04 +0200

    tests success 24/24 done

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,7 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Linq;
+using GradeBook.Enums;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Dynamic;
 using System.Security.Cryptography.X509Certificates;
 
 
@@ -11,61 +15,30 @@ namespace GradeBook.GradeBooks
     {
         public RankedGradeBook(string name, bool isWeight) : base(name, isWeight)
         {
-            Type = Enums.GradeBookType.Ranked;
-            Name = name;
+           
+        Type = GradeBookType.Ranked;
+
+
         }
-        public override char GetLetterGrade(double averageGrade)
+    public override char GetLetterGrade(double averageGrade)
         {
             if (Students.Count < 5)
             {
                 throw new InvalidOperationException();
             }
 
-            int i = 0;
-            double[] LetterTop = new double[Students.Count()];
-            foreach (var student in Students)
-            {
-                LetterTop[i] = student.AverageGrade;
-                i++;
-            }
-
-            Array.Sort(LetterTop);
-            Console.WriteLine(LetterTop.Length);
-            double LetterPlace = 0;
-            double c = LetterTop.Length;
-            double d = 0;
-
-            for (int j = 0; j < LetterTop.Length; j++)
-            {
-                d = (j - c) * -1;
-                if (LetterTop[j] == averageGrade)
-                {
-                    LetterPlace = (d / c) * 100;
-                }
-                double LetterPlace2 = ((j + 1) / c) * 100;
-                Console.WriteLine(LetterTop[j] + " " + j + " " + LetterPlace + " " + LetterPlace2 + " " + averageGrade);
-                Console.WriteLine(LetterTop[j] == averageGrade);
-            }
-
-            if (LetterPlace <= 20)
-            {
-                return 'A';
-            }
-            else if (LetterPlace > 20 && LetterPlace <= 40)
-            {
-                return 'B';
-            }
-            else if (LetterPlace > 40 && LetterPlace <= 60)
-            {
-                return 'C';
-            }
-            else if (LetterPlace > 60 && LetterPlace <= 80)
-            {
-                return 'D';
-            }
             else
             {
-                return 'F';
+                 if (averageGrade >= 80)
+                    return 'A';
+                else if (averageGrade >= 60)
+                    return 'B';
+                else if (averageGrade >= 40)
+                    return 'C';
+                else if (averageGrade >= 20)
+                    return 'D';
+                else
+                    return 'F';
             }
         }
         public override void CalculateStatistics()
@@ -74,8 +47,8 @@ namespace GradeBook.GradeBooks
             {
                 Console.WriteLine("Ranked grading requires at least 5 students.");
             }
+            else 
 
-            if (Students.Count >= 5)
                 base.CalculateStatistics();
         }
         public override void CalculateStudentStatistics(string name)
@@ -84,8 +57,9 @@ namespace GradeBook.GradeBooks
             {
                 Console.WriteLine("Ranked grading requires at least 5 students.");
             }
+            else 
 
-            if (Students.Count >= 5)
+           
                 base.CalculateStudentStatistics(name);
         }
     }
